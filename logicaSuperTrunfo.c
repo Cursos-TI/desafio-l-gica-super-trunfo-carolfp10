d1674fbcffb60f7cd884092f19b338002abe4fcb
carolfp10 <carol.pestili@gmail.com>
2025-03-22 00:34:45 +0000

    coloquei break e as opcoes, junto com default

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
 
     float populacao, populacao2;
-    double area, area2;
-    double pib, pib2;
-    int pontosturisticos, pontosturisticos2;
+    double area, area2, densidade, pibpercapta;
+    double pib, pib2, densidade2, pibpercapta2;
+    int pontosturisticos, pontosturisticos2, opcao, atributo;
     char nome[80], nome2[80];
 
 
@@ -40,24 +35,100 @@ scanf("%d", &pontosturisticos);
 printf("Quantos pontos turisticos da 2 cidade?:\n");
 scanf("%d", &pontosturisticos2);
 
-printf("Nome da 1 cidade: %s\n", nome);
-printf("População da 1 cidade: %f\n", populacao);
-printf("Àrea da 1 cidade: %e\n", area);
-printf("PIB da 1 cidade: %e\n", pib);
-printf("Pontos turisticos da 1 cidade: %d\n", pontosturisticos);
 
-printf("Nome da 2 cidade: %s\n", nome2);
-printf("População da 2 cidade: %f\n", populacao2);
-printf("Àrea da 2 cidade: %e\n", area2);
-printf("PIB da 2 cidade: %e\n", pib2);
-printf("Pontos turisticos da 2 cidade: %d\n", pontosturisticos2);
+densidade= (double)(populacao / area);
 
-if (populacao > populacao2) {
-    printf("%s Ganhou, pois tem a maior populacao", nome);
-}   else {
-    printf("%s Ganhou, pois tem a maior populacao", nome2);
+ pibpercapta= (double)(pib / populacao);
+
+ densidade2= (double)(populacao2 / area2);
+
+ pibpercapta2= (double)(pib2 / populacao2);
+
+printf("***Jogo Trunfo***\n");
+printf("Escola a opção desejada.\n");
+printf("1.Iniciar jogo.\n");
+printf("2. Regras.\n");
+printf("3. Sair do jogo.\n");
+scanf("%d", &opcao);
+
+switch (opcao)
+{
+case 1:
+printf("***Qual atributo deseja usar para comparação:***\n");
+printf("1. População\n");
+printf("2.Area\n");
+printf("3.Pib\n");
+printf("4.Numero de pontos turisticos\n");
+printf("5.Densidade\n");
+scanf("%d", &atributo);
+
+switch (atributo)
+{
+    case 1:
+    printf("Cidade %s vs Cidade %s \n", nome, nome2);
+    printf("População de %f vs População de %f \n", populacao, populacao2);
+    if (populacao > populacao2){
+    printf("Cidade %s venceu!\n", nome);
+    }else if (populacao2 > populacao){
+    printf("Cidade %s venceu!\n", nome2);
+    }else if (populacao == populacao2){
+        printf("Deu empate");
+    }
+    break;
+    case 2:
+    printf("Cidade %s vs Cidade %s \n", nome, nome2);
+    printf("Area de %e vs Area de %e \n", area, area2);
+    if (area > area2){
+    printf("Cidade %s venceu!\n", nome);
+    }else if (area2 > area){
+    printf("Cidade %s venceu!\n", nome2);
+    }else if (area == area2){
+        printf("Deu empate");
+    }
+    break;
+    case 3:
+    printf("Cidade %s vs Cidade %s \n", nome, nome2);
+    printf("Pib de %e vs Pib de %e \n", pib, pib2);
+    if (pib > pib2){
+    printf("Cidade %s venceu!\n", nome);
+    }else if (pib2 > pib){
+    printf("Cidade %s venceu!\n", nome2);
+    }else if (pib == pib2){
+        printf("Deu empate");
+    }
+    break;
+    case 4:
+    printf("Cidade %s vs Cidade %s \n", nome, nome2);
+    printf("Ponto turistico de %d vs Ponto turistico de %d \n", pontosturisticos, pontosturisticos2);
+    if (pontosturisticos > pontosturisticos2){
+    printf("Cidade %s venceu!\n", nome);
+    }else if (pontosturisticos2 > pontosturisticos){
+    printf("Cidade %s venceu!\n", nome2);
+    }else if (pontosturisticos == pontosturisticos2){
+        printf("Deu empate");
+    }
+    break;
+    case 5:
+    printf("Cidade %s vs Cidade %s \n", nome, nome2);
+    printf("Densidade de %e vs Densidade de %e \n", densidade, densidade2);
+    if (densidade > densidade2){
+    printf("Cidade %s venceu!\n", nome2);
+    }else if (densidade2 > densidade){
+    printf("Cidade %s venceu!\n", nome);
+    }else if (densidade == densidade2){
+        printf("Deu empate");
+    }
 }
-  
-  
+break;
+case 2:
+printf("A regra geral é: vence a carta com o maior valor no atributo escolhido.");
+printf("Porém, para a Densidade Demográfica, a regra inverte: vence a carta com o menor valor.\n");
+break;
+case 3:
+printf("Saindo...\n");
+break;
+default:
+printf("Opção inválida.");
+}
     return 0;
 }
